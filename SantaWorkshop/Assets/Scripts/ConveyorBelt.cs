40fe125cd9e45f9f49cd8768962f5c8ccac7da95
kufliclicks <kufliclicks@outlook.com>
2023-11-28 22:40:37 -0700

    added Materials, Prefabs, Scripts, and Assets folders added ConveyorBelt prefab

---
+++ SantaWorkshop/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public enum BeltDirection
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    [SerializeField]
+    public BeltDirection ConveyorBeltDirection = BeltDirection.None;
+
+	[SerializeField]
+	private float TEST_Speed = 1.0f;
+
+	private void Update()
+	{
+		var beltRenderer = gameObject.GetComponent<Renderer>();
+		if (beltRenderer == null)
+		{
+			return;
+		}
+
+		// move the tex coords to simulate belt scrolling
+		// TODO: remove when animation exists
+		Vector2 newOffset = beltRenderer.material.mainTextureOffset;
+		float offsetMagnitude = TEST_Speed * Time.deltaTime;
+
+		switch (ConveyorBeltDirection)
+		{
+			case BeltDirection.Left:
+				newOffset.x -= offsetMagnitude;
+				break;
+			case BeltDirection.Right:
+				newOffset.x += offsetMagnitude;
+				break;
+			case BeltDirection.Up:
+				newOffset.y += offsetMagnitude;
+				break;
+			case BeltDirection.Down:
+				newOffset.y -= offsetMagnitude;
+				break;
+		}
+
+		beltRenderer.material.mainTextureOffset = newOffset;
+	}
+
+	/// <summary>
+	/// Moves collided object across the belt.
+	/// </summary>
+	private void OnCollisionStay(Collision objectOnBelt)
+    {
+        Vector3 newPos = objectOnBelt.collider.transform.position;
+        float forceMagnitude = TEST_Speed * Time.deltaTime;
+
+		switch (ConveyorBeltDirection)
+        {
+            case BeltDirection.Left:
+				newPos.x -= forceMagnitude;
+                break; 
+            case BeltDirection.Right:
+				newPos.x += forceMagnitude;
+				break; 
+            case BeltDirection.Up:
+				newPos.z += forceMagnitude;
+				break; 
+            case BeltDirection.Down:
+				newPos.z -= forceMagnitude;
+				break;
+        }
+
+		objectOnBelt.collider.transform.position = newPos;
+    }
+}
