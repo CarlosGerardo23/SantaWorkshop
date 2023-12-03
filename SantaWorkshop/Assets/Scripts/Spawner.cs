a01d5586973eb264a8726b311da01384405f8e86
AtomicRider13 <143289316+AtomicRider13@users.noreply.github.com>
2023-12-02 20:38:05 -0800

    Created Spawner
    Spawner object, script, and simulated toy parts that are instantiated out of the spawner object onto the conveyor belt and ends in a trash bin. Needs a destroy object script to destroy trashed objects.

--- SantaWorkshop/Assets/Scripts/Spawner.cs
+++ SantaWorkshop/Assets/Scripts/Spawner.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] private GameObject[] toyParts;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnToyParts", 0, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnToyParts()
+    {
+        Instantiate(toyParts[Random.Range(0, toyParts.Length)], transform.position, transform.rotation);
     }
 }
