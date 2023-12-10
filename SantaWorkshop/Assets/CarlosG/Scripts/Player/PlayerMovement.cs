1e0bf35ae310074be18b56d3d37d3ced66681a7d
Carlos <carlos.enriquez.valerio@gmail.com>
2023-12-10 12:39:03 -0600

    Falling bug

--- SantaWorkshop/Assets/CarlosG/Scripts/Player/PlayerMovement.cs
+++ SantaWorkshop/Assets/CarlosG/Scripts/Player/PlayerMovement.cs
@@ -10,13 +10,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _characterRotationVelocity = 0.1f;
     private CharacterController _characterController;
     [SerializeField] private Animator playerAnimator;
-
+    [SerializeField] private float _gravity;
+    [SerializeField] private Transform[] _feets;
+    [SerializeField] private float _feetDistance;
+    private float _yVelocity = 0;
     private Vector3 _currentMovement;
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
-
-        
     }
     private void OnEnable()
     {
@@ -31,17 +32,46 @@ public class PlayerMovement : MonoBehaviour
     {
         playerAnimator.SetBool("isRunning", false);
         playerAnimator.SetBool("isIdle", true);
-
-        if (_currentMovement != Vector3.zero)
+        if (!IsFalling())
         {
-            playerAnimator.SetBool("isRunning", true);
-            playerAnimator.SetBool("isIdle", false);
-            _characterController.Move(_currentMovement * Time.deltaTime * _characterMovementVelocity);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_currentMovement), _characterRotationVelocity);
+
+            if (_currentMovement != Vector3.zero)
+            {
+                playerAnimator.SetBool("isRunning", true);
+                playerAnimator.SetBool("isIdle", false);
+                _characterController.Move(_currentMovement * Time.deltaTime * _characterMovementVelocity);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_currentMovement), _characterRotationVelocity);
+            }
         }
     }
     private void OnMove(Vector2 movement)
     {
         _currentMovement = new Vector3(movement.x, 0, movement.y).normalized;
+    }
+    private bool IsFalling()
+    {
+        _yVelocity = 0;
+        foreach (var feet in _feets)
+        {
+            Debug.Log("Feets Position: " + feet.position);
+            Debug.Log("Ray Destination: " + (-feet.transform.up * _feetDistance));
+            Vector3 rayDestination = feet.TransformDirection(-Vector3.up) * _feetDistance;
+            Debug.DrawLine(feet.position, feet.position + rayDestination, Color.green);
+            Ray ray = new Ray(feet.position, -feet.transform.up);
+            if (!Physics.Raycast(ray, _feetDistance))
+            {
+                _yVelocity += _gravity * Time.deltaTime;
+                rayDestination = feet.TransformDirection(-Vector3.up) * _feetDistance;
+                Debug.DrawLine(feet.position, feet.position + rayDestination, Color.red);
+                break;
+            }
+            else
+                _yVelocity = 0;
+
+            _characterController.Move(new Vector3(0, _yVelocity, 0));
+        }
+
+        return _yVelocity != 0;
+
     }
 }
