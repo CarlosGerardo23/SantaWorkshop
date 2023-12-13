e986d2551a12c34cb57523de2f3675306d2a3e41
AtomicRider13 <143289316+AtomicRider13@users.noreply.github.com>
2023-12-12 18:44:42 -0800

    MOre updates

---
+++ SantaWorkshop/Assets/CarlosG/Scripts/ButtonFunctions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+
+    }
+
+}
