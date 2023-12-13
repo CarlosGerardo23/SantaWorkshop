1d45307883cc97d07b63c5eeaa0f8a60b1b27967
AtomicRider13 <143289316+AtomicRider13@users.noreply.github.com>
2023-12-13 10:09:17 -0800

    Added countdown clock

---
+++ SantaWorkshop/Assets/CarlosG/Scripts/TimeUntilChristmasCountdown.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TimeUntilChristmasCountdown : MonoBehaviour
+{
+
+[SerializeField] private float totalTime = 300f;
+[SerializeField] private TextMeshProUGUI countdownTimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ensure time doesn't go below zero
+        totalTime = Mathf.Max(0, totalTime - Time.deltaTime);
+
+        // Calculate minutes and seconds
+        int minutes = Mathf.FloorToInt(totalTime / 60);
+        int seconds = Mathf.FloorToInt(totalTime % 60);
+
+        // Display the countdown
+        countdownTimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        // Optionally: You can trigger an event or perform an action when the countdown reaches zero.
+        if (totalTime <= 0)
+        {
+            totalTime = 0;
+            Debug.Log("Time has run out!");
+        }
+    }
+}
