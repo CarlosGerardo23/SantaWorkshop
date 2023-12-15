870dd870cb81578680ec1138a1d2de6e818c696c
Carlos <carlos.enriquez.valerio@gmail.com>
2023-12-14 21:45:30 -0600

    gift

---
+++ SantaWorkshop/Assets/CarlosG/Scripts/PlayerData/PlayerScoreController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerScoreController : MonoBehaviour
+{
+   [SerializeField] private string _giftTag;
+
+   private void OnTriggerEnter(Collider other)
+   {
+    
+   }
+}
